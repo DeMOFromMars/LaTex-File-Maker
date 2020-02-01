2ce8588c854125069e4eaf47aeb006c5ea156c54
MasterOfAllEvil <devinmobrien98@gmail.com>
2020-02-01 17:23:06 -0500

    Update Working

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-
 fileName = input("Please Enter File Name (no extension): ")
 file = open(fileName + ".tex", "w+")
 file.write("\\documentclass[12pt]{article}\n\n")
 
-title = input("Title: " )
-file.write("\\title{"+title +"}\n")
+title = input("Title: ")
+file.write("\\title{" + title + "}\n")
 author = input("Author: ")
-file.write("\\author{"+author+"}\n")
+file.write("\\author{" + author + "}\n")
 file.write("\\begin{document}\n")
 sopUser = input("Would you like sections or paragraphs?(s/p)")
 
@@ -16,11 +15,24 @@ num = range(0, number)
 
 if sopUser == "s" or sopUser == "S":
     for x in num:
-        file.write("\\section{Problem "+ str(x + 1) + "}\n\n")
+        file.write("\\section{Problem " + str(x + 1) + "}\n\n")
 
 if sopUser == "p" or sopUser == "P":
     for x in num:
         file.write("\\paragraph{Problem " + str(x + 1) + "}\n\n")
 
+
+for x in range(0, number):
+    file.write("\\section{Problem " + str(x + 1) + "}\n\n")
+    secondary = 0
+    secondary = int(input("How many subsections?"))
+    char = "a"
+    i = ord(char[0])
+
+    for x in range(0, secondary):
+
+        char = chr(i)
+        file.write("\\subsection{" + char + ".)}\n\n")
+        i += 1
 file.write("\\end{document}")
 file.close()
