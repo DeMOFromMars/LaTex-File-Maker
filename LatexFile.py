c50b4d76597afe429722c490cadf70b267637d06
Devin <devinmobrien1998@gmail.com>
2020-02-02 16:02:58 -0500

    Update LatexFile.py

--- LatexFile.py
+++ LatexFile.py
@@ -2,30 +2,34 @@
 # Currently Does not doe anything
 
 class Head:
-    docClass = "article"  # Currently Only Working With Article Class Type
-    mod = "12pt"
-    title = "Testing"
-    author = "Tester"
-    date = "2/1/2020"
 
     def createDocClass(self):
-        return "\\documentclass[" + mod + "]{" + docClass + "}\n"
+        return '\\documentclass[' + mod + ']{' + docClass + '}\n'
 
     def createTitle(self):
-        return "\\title{" + title + "}\n"
+        return '\\title{' + self.title + '}\n'
 
     def createAuthor(self):
-        return "\\author{" + author + "}\n"
+        return '\\author{' + str(self.author) + '}'
 
     def createDate(self):
-        return "\\date{" + date + "}\n"
+        return '\\date{' + date + '}\n'
 
-    def createHead(self):
-        createDocClass()
-        createTitle()
-        createAuthor()
-        createDate()
+    def getAuthor(self):
+        return str(self.author)
+
+    def __init__(self, doctype = "article", mod = "12pt", title = "Test", author = "tester", date = "2/2/2020"):
+        self.doctype = doctype
+        self.title = title
+        self.author = author
+        self.mod = mod
+        self.date = date
+
+    def __str__(self):
+        return "Title: " + self.title + "; Author: " + self.author + "; Date: " + self.date + "; DocType: " + self.doctype + "; Modifiers: " + self.mod
 
 
-class Body:
-    sections
+current = Head()
+print(str(current))
+
+
