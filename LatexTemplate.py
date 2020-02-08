2f2f932914bdbe96770a6b8f7391a1e3b2654060
MasterOfAllEvil <devinmobrien98@gmail.com>
2020-02-08 11:24:14 -0500

    Update LatexTemplate.py

--- LatexTemplate.py
+++ LatexTemplate.py
@@ -94,42 +94,4 @@ def load():
 
 
 start()
-=======
-
-fileName = input("Please Enter File Name (no extension): ")
-file = open(fileName + ".tex", "w+")
-file.write("\\documentclass[12pt]{article}\n\n")
-
-title = input("Title: ")
-file.write("\\title{" + title + "}\n")
-author = input("Author: ")
-file.write("\\author{" + author + "}\n")
-file.write("\\begin{document}\n")
-sopUser = input("Would you like sections or paragraphs?(s/p/a)")
-
-number = int(input("How many?"))
-
-num = range(0, number)
-
-if sopUser == "s" or sopUser == "S":
-    for x in num:
-        file.write("\\section{Problem " + str(x + 1) + "}\n\n")
-
-if sopUser == "p" or sopUser == "P":
-    for x in num:
-        file.write("\\paragraph{Problem " + str(x + 1) + "}\n\n")
-if sopUser == "a" or sopUser == "A":
-    for x in range(0, number):
-        file.write("\\section{Problem " + str(x + 1) + "}\n\n")
-        secondary = 0
-        secondary = int(input("How many subsections?"))
-        char = "a"
-        i = ord(char[0])
-
-        for x in range(0, secondary):
-            char = chr(i)
-            file.write("\\subsection{" + char + ".)}\n\n")
-            i += 1
-file.write("\\end{document}")
-file.close()
 
